aadde7893e897e4cc9c289374a6c9c9ab1e7da7c
DOUGLAS NACBAR <NACBAR@WASATAIN-PC>
2023-01-30 23:56:04 -0300

    EVENT - ADDS GENERIC EVENT HANDLER PT-02

--- ASYNCAPPLICATION/Program.cs
+++ ASYNCAPPLICATION/Program.cs
@@ -2,33 +2,33 @@
 
 int.TryParse(args[0], out int complexTaskValue);
 
-var listOfTask = new List<Task<string>>();
-var genericTaskEventHandler = new GenericTaskEventHandler<string>();
+var listOfTask = new List<Task<object>>();
+var genericTaskEventHandler = new GenericTaskEventHandler<object>();
 genericTaskEventHandler.GenericTaskEvent += GenericTaskEventHandler_GenericTaskEvent;
 
 if (complexTaskValue > decimal.Zero)
 {
     Console.WriteLine("Tarefa Complicada Iniciada...");
-    listOfTask.Add(Task.Delay(complexTaskValue).ContinueWith(x => { return "Tarefa Complicada Finalizada!"; }));
+    listOfTask.Add(Task.Delay(complexTaskValue).ContinueWith(x => { return (object)"Tarefa Complicada Finalizada!"; }));
 }
 
 Console.WriteLine("Tarefa Maior Iniciada...");
-listOfTask.Add(Task.Delay(10000).ContinueWith(x => { return "Tarefa Maior Finalizada!"; }));
+listOfTask.Add(Task.Delay(10000).ContinueWith(x => { return (object)"Tarefa Maior Finalizada!"; }));
 
 Console.WriteLine("Tarefa Média Iniciada...");
-listOfTask.Add(Task.Delay(7500).ContinueWith(x => { return "Tarefa Média Finalizada!"; }));
+listOfTask.Add(Task.Delay(7500).ContinueWith(x => { return (object)"Tarefa Média Finalizada!"; }));
 
 Console.WriteLine("Tarefa Menor Iniciada...");
-listOfTask.Add(Task.Delay(5000).ContinueWith(x => { return "Tarefa Menor Finalizada!"; }));
+listOfTask.Add(Task.Delay(5000).ContinueWith(x => { return (object)"Tarefa Menor Finalizada!"; }));
 
 Console.WriteLine();
 
 await genericTaskEventHandler.TriggerGenericTaskEvent(listOfTask);
 
-Console.WriteLine(Environment.NewLine + "Todas Tarefas Estão Concluídas!");
+Console.WriteLine(Environment.NewLine + "Todas Tarefas Finalizadas!");
 Console.ReadLine();
 
-void GenericTaskEventHandler_GenericTaskEvent(object? sender, GenericTaskEventArgs<string> e)
+static void GenericTaskEventHandler_GenericTaskEvent(object? sender, GenericTaskEventArgs<object> e)
 {
     Console.WriteLine(e.TaskEventResult);
 }
---
+++ SYNCAPPLICATION/Program.cs
@@ -0,0 +1,46 @@
+﻿_ = bool.TryParse(args[0], out bool syncExecution);
+_ = int.TryParse(args[1], out int complexTaskValue);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+if (syncExecution)
+{
+    Console.WriteLine("Tarefa Complicada Iniciada...");
+    Thread.Sleep(complexTaskValue);
+    Console.WriteLine("Tarefa Complicada Finalizada!");
+
+    Console.WriteLine("Tarefa Maior Iniciada...");
+    Thread.Sleep(5000);
+    Console.WriteLine("Tarefa Maior Finalizada!");
+
+    Console.WriteLine("Tarefa Média Iniciada...");
+    Thread.Sleep(4000);
+    Console.WriteLine("Tarefa Média Finalizada!");
+
+    Console.WriteLine("Tarefa Menor Iniciada...");
+    Thread.Sleep(3000);
+    Console.WriteLine("Tarefa Menor Finalizada!");
+}
+else
+{
+    Console.WriteLine("Tarefa Complicada Iniciada...");
+    await Task.Delay(complexTaskValue);
+    Console.WriteLine("Tarefa Complicada Finalizada!");
+
+    Console.WriteLine("Tarefa Maior Iniciada...");
+    await Task.Delay(5000);
+    Console.WriteLine("Tarefa Maior Finalizada!");
+
+    Console.WriteLine("Tarefa Média Iniciada...");
+    await Task.Delay(4000);
+    Console.WriteLine("Tarefa Média Finalizada!");
+
+    Console.WriteLine("Tarefa Menor Iniciada...");
+    await Task.Delay(3000);
+    Console.WriteLine("Tarefa Menor Finalizada!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Todas Tarefas Finalizadas!");
+Console.ReadLine();
